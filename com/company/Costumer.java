72c2d987926d63a6150e8220fc78b089e3461d7a
xenius <xeni0103@stud.kea.dk>
2021-11-10 10:14:31 +0100

    Costumer basis

--- com/company/Costumer.java
+++ com/company/Costumer.java
@@ -1,5 +1,29 @@
 package com.company;
 
 public class Costumer {
+    String name;
+    String address;
+    int postNr;
+    String city;
+    int mobilePhone;
+    int phone;
+    String email;
+
+    public Costumer(String name, String address, int postNr, String city, int mobilePhone, int phone, String email){
+        this.name = name;
+        this.address = address;
+        this.postNr = postNr;
+        this.city = city;
+        this.mobilePhone = mobilePhone;
+        this.phone = phone;
+        this.email = email;
+    }
+    public Costumer(){
+
+    }
+    public String toString() {
+        return ("Name: " +name+ "\nAddress: "+address+ "\nPostal code: "+postNr+ "\nCity: "+city+
+                "\nMobile phone number: "+mobilePhone +"\nPhone number: "+phone +"\nE-mail: "+email);
+    }
 
 }
