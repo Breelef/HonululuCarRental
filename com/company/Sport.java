3f9787f51689964f103e36d69742ff96b8d9d257
breelef <emil_vinther@hotmail.com>
2021-11-10 10:19:52 +0100

    Honululu biler

--- com/company/Sport.java
+++ com/company/Sport.java
@@ -1,5 +1,25 @@
 package com.company;
 
-public class Sport {
+public class Sport extends Vehicle{
+    boolean gear;
+    int HP;
 
+    public Sport(String b, String m, String f, int nr, int year, int month, boolean g, int hp){
+        super(b, m, f, nr, year, month);
+        gear = g;
+        HP = hp;
+    }
+
+    public Sport(){
+    }
+    @Override
+    public String toString(){
+        String g = "";
+        if(gear){
+            g = "Manuel gear";
+        }else
+            g = "Automat gear";
+        return ("Mærke: "+brand+"\nModel: "+model+"\nBenzin type: "+fuel+"\nGear type: "+g+
+                "\nHorse Power: "+HP+"\nRegistreringsnummer: "+regNr+"\nRegistreringsdato: "+regMonth+"/"+regYear);
+    }
 }
--- com/company/Vehicle.java
+++ com/company/Vehicle.java
@@ -1,5 +1,26 @@
 package com.company;
 
 public class Vehicle {
+    String brand;
+    String model;
+    String fuel;
+    int regNr;
+    int regYear;
+    int regMonth;
 
+    public Vehicle(String b, String m, String f, int nr, int year, int month){
+        brand = b;
+        model = m;
+        fuel = f;
+        regNr = nr;
+        regYear = year;
+        regMonth = month;
+    }
+    public Vehicle(){
+    }
+
+    public String toString(){
+        return ("Mærke: "+brand+"\nModel: "+model+"\nBenzin type: "+fuel+"\nRegistreringsnummer: "
+                +regNr+"\nRegistreringsdato: "+regMonth+"/"+regYear);
+    }
 }
